2e8cec5dd5606af52b0049ae8943cce9c2d232e7
Julien Piguet <13250437+julienpiguet@users.noreply.github.com>
2022-01-26 09:51:00 +0100

    add vue socket.io

--- frontend/wikigame/src/main.js
+++ frontend/wikigame/src/main.js
@@ -1,5 +1,16 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import store from './store'
+import VueSocketIO from 'vue-socket.io'
+ 
+Vue.use(new VueSocketIO({
+    debug: true,
+    connection: 'http://localhost:3000',
+    vuex: {
+        store,
+        actionPrefix: 'SOCKET_',
+        mutationPrefix: 'SOCKET_'
+    }
+}))
 
 createApp(App).use(store).mount('#app')
